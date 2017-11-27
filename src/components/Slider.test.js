e1fe05f5ba3f56240d5a91722264b460e2d28b86
interglobalmedia <interglobalmedia@gmail.com>
2017-11-27 12:03:30 -0500

    Completed tweaks. Will ask for advice re best practices.

--- src/components/Slider.test.js
+++ src/components/Slider.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from './Slider';
 
 describe('<Slider />', () => {
-	it('should rendera slide, a left and right arrow, and slide indicators', function() {
+	it('should render a slide, a left and right arrow, and slide indicators', function() {
 		expect(<Slider />).toEqual(<Slider />);
 	})
 })
